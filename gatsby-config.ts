3338d8f54faece6481a9b857f42e3772bcbdb8d9
pasalino <pasalino@gmail.com>
2023-10-14 13:49:10 +0200

    add GA

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -38,6 +38,20 @@ const config: GatsbyConfig = {
       },
       __key: "pages",
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["GTM-PBF2CJPF"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
+    },
   ],
 };
 
