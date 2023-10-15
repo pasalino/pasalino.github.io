a339280bb302b1bbae02638ec49874e5257c3bfe
pasalino <pasalino@gmail.com>
2023-10-15 13:21:35 +0200

    add medium link

--- src/components/social-bar.tsx
+++ src/components/social-bar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { GitHubIcon } from "./social-icons/github-icon";
 import { LinkedinIcon } from "./social-icons/linkedin-icon";
+import { MediumIcon } from "./social-icons/medium-icon";
 
 export const SocialBar = () => (
   <div className="bg-blue rounded-b-lg flex flex-row min-h-[50px] justify-center items-center fill-white">
@@ -9,6 +10,7 @@ export const SocialBar = () => (
         href={"https://github.com/pasalino"}
         target="_blank"
         className=" fill-white"
+        title="GitHub"
       >
         <GitHubIcon />
       </Link>
@@ -16,8 +18,17 @@ export const SocialBar = () => (
         href={"https://www.linkedin.com/in/pasalino/"}
         target="_blank"
         className=" fill-white"
+        title="Linkedin"
       >
         <LinkedinIcon />
+      </Link>
+      <Link
+        href={"https://medium.com/@pasalino"}
+        target="_blank"
+        className=" fill-white"
+        title="Medium"
+      >
+        <MediumIcon />
       </Link>
     </>
   </div>
