7239a2aef7cc0fe81ba21cc61cc2906ba2c02848
pasalino <pasalino@gmail.com>
2023-10-21 15:49:20 +0200

    chore: GA4

--- src/components/gtag.tsx
+++ src/components/gtag.tsx
@@ -2,14 +2,14 @@ import Script from "next/script";
 
 export const GTag = () => (
   <>
-    <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-PBF2CJPF" />
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-V596HZP86Z" />
     <Script id="google-analytics">
       {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', 'GTM-PBF2CJPF');
+          gtag('config', 'G-V596HZP86Z');
         `}
     </Script>
   </>
